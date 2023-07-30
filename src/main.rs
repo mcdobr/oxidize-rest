0df1c338492b6340c386f719f364a302464c4e12
mcdobr <mircea.dobreanu@gmail.com>
2023-07-30 20:59:17 +0300

    Add serde for json serialization

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Serialize};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Something")
+#[derive(Serialize)]
+pub struct Response {
+    pub message: String,
 }
+
+#[get("/articles")]
+async fn find_articles() -> impl Responder {
+    let response = Response {
+        message: "Something".to_string()
+    };
+    HttpResponse::Ok().json(response)
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new().service(find_articles))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
